99f0478d4d25fd3460f1a0baa8abf78aeb2e195c
Connor Rigby <connor@emeraldkingdomgreenhouse.com>
2016-07-25 12:35:56 -0700

    rtl wifi

--- nerves.exs
+++ nerves.exs
@@ -9,8 +9,7 @@ config :nerves_system_rpi2, :nerves_env,
   type:  :system,
   version: version,
   mirrors: [
-    "https://github.com/nerves-project/nerves_system_rpi2/releases/download/v#{version}/nerves_system_rpi2-v#{version}.tar.gz",
-    "https://s3.amazonaws.com/nerves/artifacts/nerves_system_rpi2-#{version}.tar.gz"],
+    "https://github.com/PressY4EKG/nerves_system_rpi2/releases/download/v0.6.0-rtl-wifi-mod/nerves_system_rpi2.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
